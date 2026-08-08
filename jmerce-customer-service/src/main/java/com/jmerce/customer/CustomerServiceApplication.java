3a1c155eddfcd0fcc01839c4f0e4988fc0aa2a44
Alexander Dudkin <raddanprofile@gmail.com>
2026-08-08 12:06:58 +0300

    feat: added initial customer-service configuration

---
+++ jmerce-customer-service/src/main/java/com/jmerce/customer/CustomerServiceApplication.java
@@ -0,0 +1,16 @@
+/*
+ * SPDX-FileCopyrightText: Copyright (c) 2026 Aliaksandr Dudkin
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.jmerce.customer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerServiceApplication.class, args);
+    }
+}
---
+++ jmerce-customer-service/src/main/resources/db/migration/V0__init_schema.sql
@@ -0,0 +1,3 @@
+CREATE SCHEMA IF NOT EXISTS profile;
+
+SET search_path TO profile, pg_catalog;
