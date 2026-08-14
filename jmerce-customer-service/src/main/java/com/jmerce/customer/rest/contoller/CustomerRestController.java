7616154090710c54f49cea31dad563e02046169c
Alexander Dudkin <raddanprofile@gmail.com>
2026-08-14 15:34:48 +0300

    fix: removed unused import

--- jmerce-customer-service/src/main/java/com/jmerce/customer/rest/contoller/CustomerRestController.java
+++ jmerce-customer-service/src/main/java/com/jmerce/customer/rest/contoller/CustomerRestController.java
@@ -16,7 +16,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import java.net.URI;
 import java.util.UUID;
 
 @RestController
